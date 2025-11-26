f0cfbae477d2076ecaba417d5224c50ef8569d57
Jorigto091 <hogojorsj@gmail.com>
2025-11-25 18:03:32 -0700

    Initial Commit

---
+++ Lyra/LyraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyraApp.swift
+//  Lyra
+//
+//  Created by Podz on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LyraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
